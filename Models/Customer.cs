ebeb4dbfa2f7a29fc29eaaa61b785b37a34fdb4f
ketki000 <ketki.patil2019@vitbhopal.ac.in>
2024-06-07 13:14:50 +0530

    initial commit

---
+++ Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace PlantNurseryAssessment.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+
+        public string ? Name { get; set; } =  "string";
+        [Phone]
+        public string? PhoneNumber { get; set; } = "0000000000";
+        public DateTime CreatedOn { get; set; } = DateTime.Now.ToUniversalTime();
+       
+    }
+}
---
+++ Models/Plant.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlantNurseryAssessment.Models
+{
+    public enum SaleStatus
+    {
+        AVAILABLE,
+        SOLD
+
+    }
+    public class Plant
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Name must be between 1 and 50 characters.")]
+        public string Name { get; set; }
+        [Required]
+        [Range(0, double.MaxValue, ErrorMessage = "Price must be a positive number.")]
+        public int Price { get; set; }
+        
+        public SaleStatus SaleStatus { get; set; } = SaleStatus.AVAILABLE;
+        public DateTime CreatedOn { get; set; } = DateTime.Now.ToUniversalTime();
+        public DateTime UpdatedOn { get; set; } = DateTime.Now.ToUniversalTime();
+        //public int? CustomerId { get; set; }
+        //public Customer? Customer { get; set; }
+    }
+}
